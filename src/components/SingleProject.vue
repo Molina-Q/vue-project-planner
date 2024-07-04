29cd7e83946f58a5d35a548da2a15db6352e9757
Molina-Q <quentinmolina1@gmail.com>
2024-07-04 23:35:06 +0200

    chore: Refactor SingleProject component for improved readability and functionality

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,41 +1,64 @@
 <template>
   <div class="project">
     <div class="actions" @click="toggleDetails">
-        <h3>{{ project.title }}</h3>
+      <h3>{{ project.title }}</h3>
+    </div>
+
+    <div class="icons">
+      <span class="material-icons"> edit </span>
+      <span class="material-icons"> delete </span>
+      <span class="material-icons"> done </span>
     </div>
 
     <div v-show="showDetails" class="details">
-        <p>{{ project.details }}</p>
+      <p>{{ project.details }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['project'],
-    data() {
-        return {
-            showDetails: false
-        }
+  props: ["project"],
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
     },
-    methods: {
-        toggleDetails() {
-            this.showDetails = !this.showDetails;
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
 .project {
-    margin: 20px auto;
-    background: white;
-    padding: 10px 20px;
-    border-radius: 4px;
-    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
-    border-left: 4px solid #e90074;
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
 }
 h3 {
+  cursor: pointer;
+}
+
+.action {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
     cursor: pointer;
+}
+
+.material-icons:hover {
+    color: #777;
 }
 </style>
