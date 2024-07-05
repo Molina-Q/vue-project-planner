5282e41d86a1bf8c428174cea3bf8e50d456d265
Molina-Q <quentinmolina1@gmail.com>
2024-07-05 20:52:32 +0200

    feat: Add Navbar component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-  </nav>
+  <Navbar />
   <router-view />
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 body {
