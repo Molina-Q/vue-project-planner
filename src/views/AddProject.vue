f931a0fc998be3f62657ba725ff83be89b9d6f40
Molina-Q <quentinmolina1@gmail.com>
2024-07-05 20:37:54 +0200

    created addProject component

---
+++ src/views/AddProject.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label for="title" >Title:</label>
+    <input type="text" id="title" v-model="title" required />
+
+    <label for="details">Details:</label>
+    <textarea id="details" v-models="details" required></textarea>
+
+    <button>Add project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      details: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+
+    }
+  }
+};
+</script>
+
+<style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+
+</style>
