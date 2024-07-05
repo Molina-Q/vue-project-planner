59f40449f7472f5912a23c57fdbe496e1e3bd8bf
Molina-Q <quentinmolina1@gmail.com>
2024-07-05 21:51:43 +0200

    feat: Add filer component in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current" />
+    <!-- current will store the value given from the the filterNav -->
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import FilterNav from "@/components/FilterNav.vue";
 import SingleProject from "@/components/SingleProject.vue";
 
 export default {
@@ -16,18 +23,20 @@ export default {
   data() {
     return {
       projects: [],
+      current: "all",
     };
   },
   components: {
     SingleProject,
+    FilterNav,
   },
   methods: {
     handleDelete(id) {
       this.projects = this.projects.filter((project) => project.id !== id);
     },
     handleComplete(id) {
-      let p = this.projects.find(project => project.id === id)
-      p.complete = !p.complete
+      let p = this.projects.find((project) => project.id === id);
+      p.complete = !p.complete;
     },
   },
   mounted() {
@@ -35,6 +44,17 @@ export default {
       .then((response) => response.json())
       .then((data) => (this.projects = data))
       .catch((error) => console.error(error));
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === "ongoing") {
+        return this.projects.filter((project) => !project.complete);
+      } else if (this.current === "completed") {
+        return this.projects.filter((project) => project.complete);
+      } else {
+        return this.projects;
+      }
+    },
   },
 };
 </script>
